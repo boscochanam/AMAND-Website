571deed1a52b6794e1a54590cfc9f08564ea8c92
boscochanam <66779287+boscochanam@users.noreply.github.com>
2023-10-06 00:36:38 +0530

    update stuff

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, url_for
 
 app = Flask(__name__)
 
@@ -38,6 +38,53 @@ def membership():
 def contact():
     current_page = '/contact'
     return render_template('contact.html', current_page=current_page)
+
+@app.route('/static/images/<path:image_filename>')
+def serve_image(image_filename):
+    return send_from_directory('static/images', image_filename)
+
+@app.route('/gallery')
+def gallery():
+    images = [
+        {
+            'url': url_for('serve_image', image_filename='cause-1.jpg'),
+            'title': "Patriot's Day 2023",
+            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
+            'date': "1 Jan 2023"
+        },
+        {
+            'url': url_for('serve_image', image_filename='cause-2.jpg'),
+            'title': "Manipur Protest",
+            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
+            'date': "1 Jan 2023"
+        },
+        {
+            'url': url_for('serve_image', image_filename='cause-3.jpg'),
+            'title': "International Women's Day",
+            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
+            'date': "1 Jan 2023"
+        },
+        {
+            'url': url_for('serve_image', image_filename='cause-4.jpg'),
+            'title': "AMAND Calendar Release 2023",
+            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
+            'date': "1 Jan 2023"
+        },
+        {
+            'url': url_for('serve_image', image_filename='cause-5.jpg'),
+            'title': "Ningol Chakouba 2023",
+            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
+            'date': "1 Jan 2023"
+        },
+        {
+            'url': url_for('serve_image', image_filename='cause-6.jpg'),
+            'title': "Patriot's Day 2022",
+            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
+            'date': "1 Jan 2023"
+        },
+        # Add more events here
+    ]
+    return render_template('events.html', images=images)
 
 if __name__ == "__main__":
     app.run()
