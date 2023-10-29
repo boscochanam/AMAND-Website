11942b969f1b8bce55fec41f068405c8a8873801
boscochanam <66779287+boscochanam@users.noreply.github.com>
2023-10-29 19:22:55 +0530

    implemented mongodb functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, send_from_directory, url_for
+from database import get_events_from_mongodb
 
 app = Flask(__name__)
 
@@ -6,7 +7,9 @@ app = Flask(__name__)
 def index():
     current_page = '/'
     print(current_page)
-    return render_template('index.html', current_page=current_page)
+    event_data = get_events_from_mongodb()
+    print(event_data)
+    return render_template('index.html', current_page=current_page, event_data=event_data)
 
 @app.route('/about')
 def about():
@@ -42,49 +45,6 @@ def contact():
 @app.route('/static/images/<path:image_filename>')
 def serve_image(image_filename):
     return send_from_directory('static/images', image_filename)
-
-@app.route('/gallery')
-def gallery():
-    images = [
-        {
-            'url': url_for('serve_image', image_filename='cause-1.jpg'),
-            'title': "Patriot's Day 2023",
-            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
-            'date': "1 Jan 2023"
-        },
-        {
-            'url': url_for('serve_image', image_filename='cause-2.jpg'),
-            'title': "Manipur Protest",
-            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
-            'date': "1 Jan 2023"
-        },
-        {
-            'url': url_for('serve_image', image_filename='cause-3.jpg'),
-            'title': "International Women's Day",
-            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
-            'date': "1 Jan 2023"
-        },
-        {
-            'url': url_for('serve_image', image_filename='cause-4.jpg'),
-            'title': "AMAND Calendar Release 2023",
-            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
-            'date': "1 Jan 2023"
-        },
-        {
-            'url': url_for('serve_image', image_filename='cause-5.jpg'),
-            'title': "Ningol Chakouba 2023",
-            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
-            'date': "1 Jan 2023"
-        },
-        {
-            'url': url_for('serve_image', image_filename='cause-6.jpg'),
-            'title': "Patriot's Day 2022",
-            'description': "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life",
-            'date': "1 Jan 2023"
-        },
-        # Add more events here
-    ]
-    return render_template('events.html', images=images)
 
 if __name__ == "__main__":
     app.run()
---
+++ database.py
@@ -0,0 +1,18 @@
+import pymongo
+
+connection_string = "mongodb+srv://amandpune:amandpunedatabase@cluster0.2n6kilc.mongodb.net/?retryWrites=true&w=majority"
+
+def connect_to_database():
+    client = pymongo.MongoClient(connection_string)
+    return client
+
+def get_events_from_mongodb():
+    client = connect_to_database()
+    db = client['AMAND']
+    collection = db['events']
+    cursor = collection.find({})
+    event_data = list(cursor)  # Convert cursor to a list of dictionaries
+    return event_data
+
+if __name__ == '__main__':
+    print(get_events_from_mongodb())
