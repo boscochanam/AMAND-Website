148cdf29fee54d73ee9074e032aa2eaedacb8db9
boscochanam <66779287+boscochanam@users.noreply.github.com>
2023-12-21 11:23:06 +0530

    fixing events

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, send_from_directory, url_for, jsonify
-from database import get_events_from_mongodb, get_news_from_mongodb, get_executives_from_mongodb, get_community_service_from_mongodb, get_blog_by_name
+from database import get_blogs_from_mongodb, get_news_from_mongodb, get_executives_from_mongodb, get_community_service_from_mongodb, get_blog_by_name
 
 
-event_data = get_events_from_mongodb()
+blog_data = get_blogs_from_mongodb()
 community_service_data = get_community_service_from_mongodb()
 executives_data = get_executives_from_mongodb()
 news_data = get_news_from_mongodb()
@@ -36,47 +36,47 @@ def create():
 @app.route('/')
 def index():
     current_page = '/'
-    return render_template('index.html', current_page=current_page, event_data=event_data, community_service_data=community_service_data)
+    return render_template('index.html', current_page=current_page, event_data=blog_data, community_service_data=community_service_data)
 
 @app.route('/about')
 def about():
     current_page = '/about'
-    return render_template('about.html', current_page=current_page, executives_data=executives_data, event_data=event_data)
+    return render_template('about.html', current_page=current_page, executives_data=executives_data, event_data=blog_data)
 
 @app.route('/events')
 def events():
     current_page = '/events'
-    return render_template('event.html', current_page=current_page, event_data=event_data, community_service_data=community_service_data)
+    return render_template('event.html', current_page=current_page, event_data=blog_data, community_service_data=community_service_data)
 
 @app.route('/publications')
 def publications():
     current_page = '/publications'
-    return render_template('publish.html', current_page=current_page, event_data=event_data)
+    return render_template('publish.html', current_page=current_page, event_data=blog_data)
 
 @app.route('/news')
 def news():
     current_page = '/news'
-    return render_template('news.html', current_page=current_page, news_data=news_data, event_data=event_data)
+    return render_template('news.html', current_page=current_page, news_data=news_data, event_data=blog_data)
 
 @app.route('/membership')
 def membership():
     current_page = '/membership'
-    return render_template('membership.html', current_page=current_page, event_data=event_data)
+    return render_template('membership.html', current_page=current_page, event_data=blog_data)
 
 @app.route('/contact')
 def contact():
     current_page = '/contact'
-    return render_template('contact.html', current_page=current_page, event_data=event_data, executives_data=executives_data)
+    return render_template('contact.html', current_page=current_page, event_data=blog_data, executives_data=executives_data)
 
 @app.route('/blog/<string:page_name>')
 def blog_page(page_name):
     # Fetch blog data
-    blog_data = get_blog_by_name(page_name)
+    blogs_data = get_blog_by_name(page_name)
     
     # Check if the blog exists
     if blog_data:
         # Pass the blog data to the template
-        return render_template('blogpage.html', date = blog_data['date'] ,title=blog_data['title'], content=blog_data['content'], images=blog_data['images'], event_data=event_data)
+        return render_template('blogpage.html', date = blogs_data['date'] ,title=blogs_data['title'], content=blogs_data['content'], images=blogs_data['images'], blogs_data=blogs_data, event_data=blog_data)
     else:
         # Render a template for a blog not found
         return render_template('blog_not_found.html')
--- database.py
+++ database.py
@@ -1,56 +1,40 @@
 import pymongo
 
 connection_string = "mongodb+srv://amandpune:amandpunedatabase@cluster0.2n6kilc.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(connection_string)
+db = client['AMAND']
 
-def connect_to_database():
-    client = pymongo.MongoClient(connection_string)
-    return client
-
-def get_events_from_mongodb():
-    client = connect_to_database()
-    db = client['AMAND']
-    collection = db['events']
+def get_blogs_from_mongodb():
+    collection = db['blogs']
     cursor = collection.find({})
-    event_data = list(cursor)  # Convert cursor to a list of dictionaries
-    return event_data
+    blog_data = list(cursor)  # Convert cursor to a list of dictionaries
+    return blog_data
+
+def get_blog_by_name(name):
+    blogs = get_blogs_from_mongodb()
+    for blog in blogs:
+        if blog['name'] == name:
+            return blog
+    return None
 
 def get_news_from_mongodb():
-    client = connect_to_database()
-    db = client['AMAND']
-    collection = db['news']  
-    query = {}  
+    collection = db['news']
+    query = {}
     cursor = collection.find(query)
-    news_data = list(cursor) 
+    news_data = list(cursor)
     return news_data
 
 def get_executives_from_mongodb():
-    client = connect_to_database()
-    db = client['AMAND']
     collection = db['executives']
     cursor = collection.find({})
     executives_data = list(cursor)  # Convert cursor to a list of dictionaries
     return executives_data
 
 def get_community_service_from_mongodb():
-    client = connect_to_database()
-    db = client['AMAND']
     collection = db['community_service']
     cursor = collection.find({})
     community_service_data = list(cursor)  # Convert cursor to a list of dictionaries
     return community_service_data
 
-def get_blog_by_name(name):
-    client = connect_to_database()
-    db = client['AMAND']
-    collection = db['blogs']
-    query = {'name': name}
-    cursor = collection.find(query)
-    blog_data = list(cursor)  # Convert cursor to a list of dictionaries
-    if blog_data:
-        return blog_data[0]
-    else:
-        return None
-
 if __name__ == '__main__':
-
     print(get_community_service_from_mongodb())
